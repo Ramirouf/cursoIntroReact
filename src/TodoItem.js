13ed2f77abc8ffdd644d36fd2e54203fb92d1cab
Ramiro Uffelmann <ramirouf@gmail.com>
2023-04-30 16:02:45 -0300

    Finished 8th class (events management)

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,13 +2,25 @@ import React from "react";
 import "./TodoItem.css"
 
 function TodoItem(props) {
+    const onComplete = () => {
+        alert(`Task ${props.text} completed`)
+    }
+    const onDelete = () => {
+        alert(`Task ${props.text} deleted`)
+    }
     return (
         <li className="TodoItem">
             {/* if completed === true --> adds class Icon-check--active to the span*/}
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+            <span
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >√</span>
             {/* if completed === true --> adds class TodoItem-p--complete to the span */}
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >X</span>
         </li>
     )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch() {
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    };
     return (
         //The value we write here, changes the TodoItems
-        <input className="TodoSearch" placeholder="sample text" />
+        <input
+            className="TodoSearch"
+            placeholder="sample text"
+            onChange={onSearchValueChange}
+        />
 
     );
 }
