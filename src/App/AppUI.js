a85e6cf00fe87a7aebb9173ed1fab52d8c78b1a9
Ramiro Uffelmann <ramirouf@gmail.com>
2023-06-07 13:00:34 -0300

    Finished 17th class (React.useContext for sharing global data in out project, instead of sending them by props)

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,6 +7,12 @@ import { TodoList } from "../TodoList";
 import { TodoSearch } from "../TodoSearch";
 
 function AppUI() {
+    //This is the value stored in our provider 
+    //const value = React.useContext(TodoContext); //Same value we would be receiving from calling our Consumer in the render props function
+
+    const { error, loading, searchedTodos, completeTodo, deleteTodo } = React.useContext(TodoContext);
+    //With useContext set, we can erase TodoContext.Consumer
+
     return (
         //React defines that only 1 element can be returned per component. That's why "ReactFragment" is used
         //ReactFragment is an invisible container
@@ -14,27 +20,24 @@ function AppUI() {
             <TodoCounter />
             <TodoSearch />
 
-            <TodoContext.Consumer>
-                {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
-                    <TodoList> {/*Container of TODOs*/}
-                        {error && <p>There was en error</p> /*If error is true, show p*/}
-                        {loading && <p>Loading...</p> /*If loading is true, show p*/}
-                        {(!loading && !searchedTodos.length) && <p>Create your first ToDo</p> /*It not loading, and searchedTodos is empty, render p*/}
+            <TodoList> {/*Container of TODOs*/}
+                {error && <p>There was en error</p> /*If error is true, show p*/}
+                {loading && <p>Loading...</p> /*If loading is true, show p*/}
+                {(!loading && !searchedTodos.length) && <p>Create your first ToDo</p> /*It not loading, and searchedTodos is empty, render p*/}
 
-                        {/* Each todo that the user will create. Will be reutilized each time an user creates a todo*/}
-                        {/*Using map to render each ToDo item in todos*/}
-                        {searchedTodos.map(todo => (
-                            <TodoItem
-                                key={todo.text}
-                                text={todo.text}
-                                completed={todo.completed}
-                                onComplete={() => completeTodo(todo.text)}
-                                onDelete={() => deleteTodo(todo.text)}
-                            />
-                        ))}
-                    </TodoList>
-                )}
-            </TodoContext.Consumer>
+                {/* Each todo that the user will create. Will be reutilized each time an user creates a todo*/}
+                {/*Using map to render each ToDo item in todos*/}
+                {searchedTodos.map(todo => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
+
 
             <CreateTodoButton /> {/*Opens the modal to create new todos in the future*/}
         </React.Fragment >
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./TodoCounter.css";
+import { TodoContext } from "../TodoContext";
 
-
-function TodoCounter({ total, completed }) {
-
+//Won't receive props anymore. Now Context will be used.
+function TodoCounter() {
+    const { completedTodos, totalTodos } = React.useContext(TodoContext);
     return (
-        <h2 className="TodoCounter">You've completed {completed} of {total} TODOs</h2>
+        <h2 className="TodoCounter">You've completed {completedTodos} of {totalTodos} TODOs</h2>
 
     )
 }
